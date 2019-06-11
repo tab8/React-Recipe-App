8dfb92c573badf13aa94612ec5eaf4eee104b034
Tom Sawyer <tomsawyer2k@gmail.com>
2019-06-11 18:08:01 +1200

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Default from "./pages/Default";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div>Hello from Recipe App</div>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          {/*navbar*/}
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default App;
