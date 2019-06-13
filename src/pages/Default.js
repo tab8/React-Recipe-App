0384cc84d52c774065761fbce23f79aec0bd6a4e
Tom Sawyer <tomsawyer2k@gmail.com>
2019-06-13 17:58:01 +1200

    Default Component

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default class Default extends Component {
   render() {
-    return <h4>Hello from the Error Page.</h4>;
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h1 className="text-light text-uppercase">page not found</h1>
+        <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+          return home
+        </Link>
+      </Header>
+    );
   }
 }
